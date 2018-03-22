0c79f252bf26ea1e2d47e58990213bc501291dc2
Sol Jennings <sol.jennings@myob.com>
2018-03-23 10:56:41 +1100

    Refactor 4th test

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace FizzBuzzKata
 {
     public class FizzBuzz
@@ -7,18 +8,28 @@ namespace FizzBuzzKata
         {
             var elements = new string[iterations];
 
+            PopulateOutputElements(iterations, elements);
+
+            return string.Join('\n', elements);
+        }
+
+        private void PopulateOutputElements(int iterations, IList<string> elements)
+        {
             for (var i = 1; i <= iterations; i++)
             {
                 if (i % 3 == 0)
                 {
                     elements[i - 1] = "Fizz";
-                    continue;
                 }
-
-                elements[i - 1] = i.ToString();
+                else if (i % 5 == 0)
+                {
+                    elements[i - 1] = "Buzz";
+                }
+                else
+                {
+                    elements[i - 1] = i.ToString();
+                }
             }
-
-            return string.Join('\n', elements);
         }
     }
 }
